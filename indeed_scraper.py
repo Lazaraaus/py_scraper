712ae60c5a9dd55a48ade4837dad2134591a0bc2
Lazaraaus <wood.yjaden@gmail.com>
2021-02-19 12:16:47 -0500

    Indeed_scraper converted to function, handles pagination

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -2,56 +2,78 @@
 import requests, pprint
 from bs4 import BeautifulSoup
 # Get Search URL 
-url = 'https://www.indeed.com/jobs?q=software+developer&l=United+States'
-url_pagination = 'https://www.indeed.com/jobs?q=software+developer&l=United+States&start=10'
+url = 'https://www.indeed.com/jobs?'
+#q=software+developer&l=United+States'
 
-# Request page
-page = requests.get(url)
+url_pagination = 'https://www.indeed.com/jobs?q=software+developer&l=United+States&start='
 
-# Parse page with BS4
-soup = BeautifulSoup(page.content, 'html.parser')
+# Define scraper function 
+def scraper(pages, location='&l=United+States', position='q=software+developer'):
+    # Created variable to track returned results and variable for pagination
+    results = (pages * 10) - 10
+    returned_results = 0
+    page_string = '&start='
+    
+    # Loop until  we've hit limit 
+    while returned_results <= results:
+        #Construct url
+        r_url = url + position + location + page_string + str(returned_results) 
 
-# Find all Divs that cointain job information 
-jobs = soup.find_all("div", class_="jobsearch-SerpJobCard")
+        # If not, Request Page 
+        page = requests.get(r_url) 
+        # DO THE SCRAPE STUFF
+        # Parse page with BS4
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-# Re-Parse page with just jobs info 
-soup2 = BeautifulSoup(str(jobs), 'html.parser')
+        # Find all Divs that cointain job information 
+        job_info = soup.find_all("div", class_="jobsearch-SerpJobCard")
 
-# Get tags with class 'company'
-companies = soup2.select(".company")
-# Get tags with class 'title'
-job_titles = soup2.select(".title > a")
-# Get tags with class 'summary' from tags with class 'jobsearch_SerpJobCard' 
-job_summaries = soup2.select(".jobsearch-SerpJobCard > .summary") # > ul") 
-# Get tags with class 'salaryText'
-job_salaries = soup2.select(".salaryText")
-# Get tags with class 'location'
-job_locations = soup2.select(".location")
+        # Re-Parse page with just jobs info 
+        soup2 = BeautifulSoup(str(job_info), 'html.parser')
+
+        # Get tags with class 'company'
+        companies = soup2.select(".company")
+        # Get tags with class 'title'
+        job_titles = soup2.select(".title > a")
+        # Get tags with class 'summary' from tags with class 'jobsearch_SerpJobCard' 
+        job_summaries = soup2.select(".jobsearch-SerpJobCard > .summary") # > ul") 
+        # Get tags with class 'salaryText'
+        job_salaries = soup2.select(".salaryText")
+        # Get tags with class 'location'
+        job_locations = soup2.select(".location")
 
  
-# Find all span tags in the DOM that have class 'salaryText'
-sal_list = soup2.find_all("span", class_="salaryText")
-# Create a dictionary to hold companies that have salary values
-comp_with_salary = {}
-# Loop through salaries in the salary list 
-for sal in sal_list:
-    # Get company info tag from salary tag 
-    company = sal.find_previous("div", class_="sjcl")
+        # Find all span tags in the DOM that have class 'salaryText'
+        sal_list = soup2.find_all("span", class_="salaryText")
+        # Create a dictionary to hold companies that have salary values
+        comp_with_salary = {}
+        # Loop through salaries in the salary list 
+        for sal in sal_list:
+            # Get company info tag from salary tag 
+            company = sal.find_previous("div", class_="sjcl")
    
-    # Get company name from company tag
-    comp_name = company.select(".company")
+            # Get company name from company tag
+            comp_name = company.select(".company")
     
-    #Add the salary to the dictionary using the company name as a key 
-    comp_with_salary[comp_name[0].text.lstrip()] = sal.text.lstrip()
+            #Add the salary to the dictionary using the company name as a key 
+            comp_with_salary[comp_name[0].text.lstrip()] = sal.text.lstrip()
 
+        # Zip together and print titles, companies, summaries, and locations
+        for i, j, k, l in zip(job_titles, companies, job_summaries, job_locations):
+            # Get key to comp_with_salary dict
+            company_key = j.text.lstrip()
+            # If key in dict, print salary info along with job info 
+            if company_key in comp_with_salary.keys():
+                print(i.text.lstrip() + "\n" + j.text.lstrip() + "\n" + comp_with_salary[company_key] + "\n" + k.text.strip() + "\n" + l.text.lstrip() + "\n")
+            # Else just print job info
+            else:
+                print(i.text.lstrip() + "\n" + j.text.lstrip() + "\n" + k.text.strip() + "\n" + l.text.lstrip() + "\n")
 
-# Zip together and print titles, companies, summaries, and locations
-for i, j, k, l in zip(job_titles, companies, job_summaries, job_locations):
-    # Get key to comp_with_salary dict
-    company_key = j.text.lstrip()
-    # If key in dict, print salary info along with job info 
-    if company_key in comp_with_salary.keys():
-        print(i.text.lstrip() + "\n" + j.text.lstrip() + "\n" + comp_with_salary[company_key] + "\n" + k.text.strip() + "\n" + l.text.lstrip() + "\n")
-    # Else just print job info
+        # Increment returned_results 
+        returned_results += 10
+    # We've hit the limit         
     else:
-        print(i.text.lstrip() + "\n" + j.text.lstrip() + "\n" + k.text.strip() + "\n" + l.text.lstrip() + "\n")
+        # WE HAVE REACHED END OF DESIRED RESULTS 
+        print("Its done!") 
+
+scraper(2)
